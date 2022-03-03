bbfc2b3462d8da2537ad594ef74e8d99edd064e3
xgaslan <gorkem_aslan0@hotmail.com>
2022-03-03 23:18:32 +0300

    General app structure has been created.

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from "expo-status-bar";
+import {StyleSheet, Text, View} from "react-native";
+import Task from "./components/Task";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App ()
+{
+    return (
+        <View style={styles.container}>
+            {/* Wrapper */}
+            <View style={styles.taskWrapper}>
+                {/* Title */}
+                <Text style={styles.sectionTitle}>Tasks</Text>
+                {/* Tasks */}
+                <View style={styles.items}>
+                    <Task text={"Task 1"} />
+                    <Task text={"Task 2"} />
+                </View>
+            </View>
+
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#E8EAED",
+    },
+    taskWrapper: {
+        paddingTop: 80,
+        paddingHorizontal: 20,
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: "bold"
+    },
+    items: {
+        marginTop: 30
+    },
 });
